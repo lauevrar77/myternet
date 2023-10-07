70637f29d54b4efcd51aec0e52576ebb64145ea0
lauevrar77 <evrardlaurent77@gmail.com>
2023-10-07 15:25:23 +0000

    feat(app): Add a webserver

--- cmd/srv/main.go
+++ cmd/srv/main.go
@@ -2,21 +2,36 @@ package main
 
 import (
 	"fmt"
-	"github.com/miekg/dns"
 	"log"
 	"myternet/domain"
 	"myternet/infrastructure/services/db"
 	mdns "myternet/infrastructure/services/dns"
+	"net/http"
 	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/miekg/dns"
+	"gorm.io/gorm"
 )
+
+func runWeb(db *gorm.DB) {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+}
 
 func main() {
 	db, err := db.Connect(domain.BlockedDNSDomain{})
 	if err != nil {
 		log.Fatalf("Failed to connect to database %s\n", err.Error())
 	}
+	go runWeb(db)
 	fmt.Println(db)
-	srv := &dns.Server{Addr: ":" + strconv.Itoa(53), Net: "udp"}
+	srv := &dns.Server{Addr: ":" + strconv.Itoa(8053), Net: "udp"}
 	srv.Handler = &mdns.Handler{}
 	if err := srv.ListenAndServe(); err != nil {
 		log.Fatalf("Failed to set udp listener %s\n", err.Error())
